4f243da2b417187caf7d668d08adb445bb76b1ba
Lauren Thorud <52000191+LalaIAm@users.noreply.github.com>
2020-04-29 19:35:30 -0500

    feat: call api

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,69 @@
-const express = require( 'express' );
-const bodyParser = require( 'body-parser' );
-const cors = require( 'cors' );
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const aylien = require('aylien_textapi');
 require( 'dotenv' ).config();
 
-app.use( express.static( 'dist' ) );
+let projectData = {};
+
+const textapi = new aylien({
+	application_id: process.env.APP_ID,
+	application_key: process.env.API_KEY,
+});
 
 
-app.get( '/', function ( req, res ) {
-    res.sendFile('dist/index.html')
-})
+app.use( bodyParser.urlencoded( { extended: false } ) )
+app.use( bodyParser.json() )
+app.use( cors() );
 
-app.listen( PORT, () => {
-    console.log( `running at http://localhost:${ PORT }` );
-})
+app.use(express.static('dist'));
+
+app.get('/', function (req, res) {
+	res.sendFile('dist/index.html');
+});
+
+app.listen(PORT, () => {
+	console.log(`running at http://localhost:${PORT}`);
+});
+
+const getTextAnalysis = (url) => {
+	return textapi.combined(
+		{
+			url: url,
+			endpoint: ['hashtags', 'summarize', 'sentiment'],
+		},
+		function (err, result) {
+			if (err === null) {
+				result.results.forEach(function (r) {
+					console.log(r.endpoint + ':');
+					console.log(r.result);
+				});
+			} else {
+				console.log(err);
+			}
+		}
+	);
+};
+
+function callBack ( req, res ) {
+    res.send( 'POST received' );
+}
+
+const analyzeUrl = async ( req, res ) => {
+    const url = req.body.url;
+    const result = await getTextAnalysis( url );
+
+    projectData.url = result;
+    return projectData;
+}
+
+const sendData = ( req, res ) => {
+    console.log( 'Data: ', projectData );
+    res.send( projectData );
+}
+
+app.post( '/add', callBack );
+app.post( '/analyze', analyzeUrl );
+app.get( '/all', sendData );
